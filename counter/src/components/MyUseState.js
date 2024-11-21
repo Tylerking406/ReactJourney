ee933fb811730ce1e26cce9fb1c4f8cbddacf0a2
Arinao Ndou <tylerking406@gmail.com>
2024-11-21 12:47:17 +0200

    decrement,reset feature and basic styling

--- counter/src/components/MyUseState.js
+++ counter/src/components/MyUseState.js
@@ -1,14 +1,35 @@
 import React, { useState } from 'react';
-
+import './styles.css';
 const MyUseState = () => {
     const [counter, setCounter] = useState(0);
-    const increment = () => (
-        setCounter(counter+1)
-    )
+
+    //increment function
+    const increment = () => {
+        if(counter< 20)
+            setCounter(counter+1)
+        else
+            window.alert("20 is our upper bound")
+    }
+
+    //decrement function
+    const decrement =()=>{
+        if( counter >0)
+            setCounter(counter-1)
+        else
+       window.alert("Why do you wanna count down to negative numbers?")
+    }
+    
+    //reset function
+    const reset =()=>{
+        setCounter(0)
+    }
   return (
     <div>
         {counter}
         <button onClick={ increment}>Increment</button>
+        <button onClick={ decrement}>Decrement</button>
+        <button onClick={ reset}>Reset</button>
+        
 
     </div>
   )
