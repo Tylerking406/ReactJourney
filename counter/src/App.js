ecfd871ef47b3cac9ef9334ca470a2cf1693cf43
Arinao Ndou <tylerking406@gmail.com>
2024-11-21 12:26:57 +0200

    react project with increment feature

---
+++ counter/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import StateHook from './components/MyUseState';
+
+function App() {
+  return (
+    <div className="App">
+      <StateHook/>
+    </div>
+  );
+}
+
+export default App;
